8b95dd280eac29dd4516c0e2cb4d26d8b7ba4594
DarshanGTHB <darshanvekariya25102004@gmail.com>
2025-06-24 10:34:14 +0530

    Jwt Set up

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,15 +1,45 @@
-export const signup = (req, res) => {
-    const {fullName, email, password} = req.body;
-    try {
-        
-        
-    } catch (error) {
-        
+import User from "../models/user.model";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        message: "Password must be at least 6 characters",
+      });
     }
-}
+
+    const user = User.findOne({ email });
+
+    if (user)
+      return res
+        .status(400)
+        .json({ success: false, message: "Email already exist1;" });
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+        
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid user data" });
+    }
+  } catch (error) {}
+};
 export const login = (req, res) => {
-    res.send("login");
-}
+  res.send("login");
+};
 export const logout = (req, res) => {
-    res.send("logout");
-}
+  res.send("logout");
+};
